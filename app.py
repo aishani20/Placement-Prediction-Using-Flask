cc631dfa540305bb54aa2a0b0117eac2033779af
Aishani Mohapatra <mohapatraaishani20@gmail.com>
2024-03-23 14:06:37 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+import os
+import sys
+import json
+from placement import make_prediction
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Get input data from the request
+        input_data = request.json
+
+        # Make prediction using the make_prediction function from placement.py
+        prediction = make_prediction(input_data["Branch"], input_data["Gender"], input_data["tenth_percentage"], 
+                                     input_data["twelfth_percentage"], input_data["CGPA_Till_sixth"], 
+                                     input_data["sixth_Sem_SGPA"], input_data["Internship"], input_data["Skills"])
+        
+        
+        
+        if (prediction == 0):
+            prediction='The person is not placed'
+        else:
+            prediction= 'The person is placed'
+        # Return the prediction as a JSON response
+        return jsonify({"prediction": prediction})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    
+    
+
+
